4a9260f20134a2daf17288d0c77e809ef26dc268
Weverson Diazz <wevers.info@gmail.com>
2020-04-15 21:07:15 -0300

    Apliquei evento no botão
    A mensagem é ativada através do botão.

--- HelloWorld/script.js
+++ HelloWorld/script.js
@@ -1,3 +1,7 @@
 var agora = new Date();
 
 const ano = document.getElementById("ano").innerHTML= agora.getFullYear();
+
+function mostrarMensagem(){
+    document.querySelector(".anuncio").classList.toggle("hide");
+}

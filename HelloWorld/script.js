122f48b777af6f65fd6621405d3c37e7db881115
Weverson Diazz <wevers.info@gmail.com>
2020-05-02 21:48:17 -0300

    novo script
    Um script js que mostra uma saudação de acordo com o horário.
    
    -Bom Dia
    -Boa Tarde
    -Boa Noite

--- HelloWorld/script.js
+++ HelloWorld/script.js
@@ -5,3 +5,19 @@ const ano = document.getElementById("ano").innerHTML= agora.getFullYear();
 function mostrarMensagem(){
     document.querySelector(".anuncio").classList.toggle("hide");
 }
+
+var hora = agora.getHours();
+
+
+if (hora >= 18) {
+    var frase = document.querySelector(".frase");frase.textContent="Boa Noite";
+    var tempo = document.querySelector(".tempo");tempo.textContent=" 🌙";
+} 
+else if (hora > 12) {
+    var frase = document.querySelector(".frase");frase.textContent="Boa Tarde";
+    var tempo = document.querySelector(".tempo");tempo.innerHTML=" ☀";
+}
+else {
+    var frase = document.querySelector(".frase");frase.textContent="Bom Dia";
+    var tempo = document.querySelector(".tempo");tempo.innerHTML=" 🌥";
+}

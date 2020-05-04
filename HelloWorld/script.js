7078c69bd0985fa2681f5a45f524f1c79ac1e0de
Weverson Diazz <wevers.info@gmail.com>
2020-05-03 22:36:52 -0300

    Estilização do box saudação

--- HelloWorld/script.js
+++ HelloWorld/script.js
@@ -8,16 +8,21 @@ function mostrarMensagem(){
 
 var hora = agora.getHours();
 
+var saudacao = document.querySelector(".saudacao");
+
 
 if (hora >= 18) {
     var frase = document.querySelector(".frase");frase.textContent="Boa Noite";
     var tempo = document.querySelector(".tempo");tempo.textContent=" 🌙";
+    saudacao.classList.add("noite");
 } 
 else if (hora > 12) {
     var frase = document.querySelector(".frase");frase.textContent="Boa Tarde";
     var tempo = document.querySelector(".tempo");tempo.innerHTML=" ☀";
+    saudacao.classList.add("tarde");
 }
 else {
     var frase = document.querySelector(".frase");frase.textContent="Bom Dia";
     var tempo = document.querySelector(".tempo");tempo.innerHTML=" 🌥";
+    saudacao.classList.add("dia");
 }
